ca8f3f2cfed7681999c4b2774f48d84ccde5cd1c
alekseysafronov <polka.pisem@gmail.com>
2023-11-19 20:37:49 +0300

    Update main.py
    While True

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from openai import OpenAI
 gptapi = 'ТВОЙ_КЛЮЧ_АПИ_ВСТАВЬ_СЮДА'
 client = OpenAI(api_key=gptapi, )
 
-print('-------------VOPROS------------->>>')
-vopros = str(input('Задай мне любой вопрос и получи крутой ответ! \n Ваш вопрос: '))
-completion = client.chat.completions.create(
-    messages=[{"role": "user", "content": vopros, }],
-    model="gpt-3.5-turbo",)
-
-print("<<<----------OTVET----------------")
-otvet = completion.choices[0].message.content
-print(otvet)
+while True:
+    print('-------------VOPROS------------->>>')
+    vopros = str(input('Задай мне любой вопрос и получи крутой ответ! \n Ваш вопрос: '))
+    completion = client.chat.completions.create(
+        messages=[{"role": "user", "content": vopros, }],
+        model="gpt-3.5-turbo",)
+    
+    print("<<<----------OTVET----------------")
+    otvet = completion.choices[0].message.content
+    print(otvet)
